4e240211a66f780419dbf84f2a52374c3caf8300
Alexander Goncharov <luzlol17@gmail.com>
2015-01-12 22:51:51 +0500

    1.0.1
    Fixing bug with GitHub Updater.

--- uwd-color-scheme.php
+++ uwd-color-scheme.php
@@ -8,7 +8,7 @@
  * Author URI: http://websanya.ru
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: https://github.com/websanya/uwd-color-scheme
+ * GitHub Plugin URI: websanya/uwd-color-scheme
  * GitHub Branch: master
  */
  
