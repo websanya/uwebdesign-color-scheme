c0b567c18f33c9f3fd945ab8ddd2cb68e9cb3ccc
Alexander Goncharov <luzlol17@gmail.com>
2016-09-16 12:13:26 +0500

    Release 3.0.0 — changed branding colors to summer 2016.

--- uwd-color-scheme.php
+++ uwd-color-scheme.php
@@ -1,27 +1,26 @@
 <?php
 /**
  * Plugin Name: uWebDesign Color Scheme
- * Plugin URI: https://github.com/websanya/uwd-color-scheme
- * Description: Микро-плагин для WordPress, который добавляет брендовую цветовую схему uWebDesign в админку.
- * Version: 2.0.0
+ * Plugin URI: https://github.com/websanya/uwebdesign-color-scheme
+ * Description: Микроплагин с цветовой схемой (WordPress админка) для комьюнити сайта uWebDesign.
+ * Version: 3.0.0
  * Author: Alexander Goncharov
  * Author URI: https://websanya.ru
- * License: GPL-2.0+
- * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
- * GitHub Plugin URI: websanya/uwd-color-scheme
+ * GitHub Plugin URI: https://github.com/websanya/uwebdesign-color-scheme
  * GitHub Branch: master
  */
- 
+
 //* Define PATH constant
 define( 'PATH', plugins_url( '', __FILE__ ) );
 
 //* Adding color scheme to WordPress admin
 function add_custom_admin_themes() {
 	wp_admin_css_color(
-    'uwd-scheme',
-    __( 'uWebDesign' ),
-    PATH . "/css/uwd-scheme.css",
-    array( '#2e2315', '#ede1d3', '#35827f', '#955152' )
+		'uwd-scheme',
+		__( 'uWebDesign' ),
+		PATH . "/css/uwd-scheme.css",
+		array( '#0e111c', '#ffffff', '#f1dd7d', '#c13223' )
 	);
 }
+
 add_action( 'admin_init', 'add_custom_admin_themes' );
